2c84e51298a1e137c1a239580503313d3ae18fbc
Stephen Woodard <stephendwoodard@protonmail.com>
2022-05-28 16:02:19 -0400

    Added clean and print statements to build script

--- etc/build_bot.bash
+++ etc/build_bot.bash
@@ -3,5 +3,8 @@
 export PROJ_ROOT="$( cd -- "$(dirname "${0}")" > /dev/null 2>&1; pwd -P | xargs dirname)"
 
 [[ -d ${PROJ_ROOT}/bin ]] || mkdir -p ${PROJ_ROOT}/bin
+[[ -f ${PROJ_ROOT}/bin/sanguinius ]] && rm -f ${PROJ_ROOT}/bin/sanguinius
+
 g++ ${PROJ_ROOT}/src/*.cpp -o ${PROJ_ROOT}/bin/sanguinius -ldpp
+echo "Build complete."
 
