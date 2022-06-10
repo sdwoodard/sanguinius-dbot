0b1485205978ae8e9b4fae5ed4ffa299d3f4bbe4
Stephen Woodard <stephendwoodard@protonmail.com>
2022-06-10 13:02:16 -0400

    Added date command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <dpp/dpp.h>
+#include <ctime>
+#include <string>
+#include <sstream>
 
 const std::string BOT_TOKEN="OTgwMDc2ODg4MTI5MTU5MTk4.GrNDbL.9L3xBQ9wIUP5v9Hw5ctD6FAN4MV87sp6cXSeoo";
 
@@ -13,9 +16,18 @@ int main()
 	{
 		if (event.command.get_command_name() == "help")
 		{
-			event.reply("I don't yet support any commands");
-			event.reply("Sorry for the inconvenience!");
-		}
+			event.reply("Right now you can only '/date' me :guraW:");
+		} 
+
+        else if (event.command.get_command_name() == "date") 
+        {
+            time_t now = time(0);
+            char* dt = ctime(&now);
+            std::ostringstream oss; 
+            oss << "It is now " << dt << " :xqcL";
+            std::string response = oss.str();
+            event.reply(response);
+        }
 	});
 
 	// register for help command
@@ -24,7 +36,8 @@ int main()
 		if (dpp::run_once<struct register_bot_commands>()) 
 		{
 			bot.global_command_create(dpp::slashcommand("help", "Provide usage", bot.me.id));
-		}
+            bot.global_command_create(dpp::slashcommand("date", "Go on a date", bot.me.id));
+        }
 	});
 
 	bot.start(false);
