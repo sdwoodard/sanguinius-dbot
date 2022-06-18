e0f6baa9bec6d10a8b9b6d3afbe78cb678383815
Stephen Woodard <stephendwoodard@protonmail.com>
2022-06-18 12:15:52 -0400

    Added two new slash commands

--- src/main.cpp
+++ src/main.cpp
@@ -39,17 +39,10 @@ int main()
 
 	bot.on_slashcommand([](const dpp::slashcommand_t& event)
 	{
+		dpp::message response_message;
 		if (event.command.get_command_name() == "help")
 		{
-			dpp::message m;
-			m.content="Right now you can only '/date' me";
-			dpp::reaction re;
-			re.emoji_name="guraW";
-			re.emoji_id=838766295616061440;
-			std::vector<dpp::reaction> res;
-			res.push_back(re);
-			m.reactions=res;
-			event.reply(m);
+			response_message.content="Help yourself, human.";
 		}
 
         else if (event.command.get_command_name() == "date")
@@ -57,10 +50,47 @@ int main()
             time_t now = time(0);
             char* dt = ctime(&now);
             std::ostringstream oss;
-            oss << "It is now " << dt << " :xqcL";
-            std::string response = oss.str();
-            event.reply(response);
+            oss << "It is now " << dt;
+            response_message.content = oss.str();
+
         }
+        else if (event.command.get_command_name() == "vote")
+        {
+        	std::string vote_response = std::get<std::string>(event.get_parameter("user_vote"));
+        	if (vote_response == "Lucas")
+        	{
+        		response_message.content = "Registered vote for Mr. New Car.";
+        	}
+        	else if (vote_response == "Nicholas")
+        	{
+        		response_message.content = "Posting " + vote_response + "'s private information now.";
+        	}
+        	else if (vote_response == "Stephen")
+        	{
+        		response_message.content = vote_response + " is immune from this dark vote.";
+        	}
+        	else
+        	{
+        		response_message.content = "ERROR: Please only vote for Lucas or Nicholas.";
+        	}
+
+        }
+        else if (event.command.get_command_name() == "repo")
+        {
+        	dpp::embed response_embed = dpp::embed().
+        		set_color(dpp::colors::sti_blue).
+				set_title("Source code for Sanguinius!").
+				set_url("https://github.com/sdwoodard/sanguinius-dbot").
+				set_author("Github", "https://github.com/", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png").
+				set_description("Code repository for all things required to build the sanguinius discord bot.").
+				set_thumbnail("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png").
+				add_field("Current regular supports:", "Stephen Woodard").
+				set_image("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png").
+				set_footer(dpp::embed_footer().set_text("Come by sometime and coding yourself!")).
+				set_timestamp(time(0));
+        	response_message.add_embed(response_embed);
+        }
+		event.reply(response_message);
 	});
 
 	// register for help command
@@ -68,8 +98,20 @@ int main()
 	{
 		if (dpp::run_once<struct register_bot_commands>())
 		{
+			dpp::slashcommand votecommand("vote", "Vote for your desired user", bot.me.id);
+			votecommand.add_option(
+				dpp::command_option(dpp::co_string, "vote_response", "The user's real name", true).
+					add_choice(dpp::command_option_choice("Lucas", std::string("user_Lucas"))).
+					add_choice(dpp::command_option_choice("Nicholas", std::string("user Nicholas"))).
+					add_choice(dpp::command_option_choice("Stephen", std::string("user Stephen")
+					)
+				)
+			);
+
 			bot.global_command_create(dpp::slashcommand("help", "Provide usage", bot.me.id));
             bot.global_command_create(dpp::slashcommand("date", "Go on a date", bot.me.id));
+            bot.global_command_create(votecommand);
+            bot.global_command_create(dpp::slashcommand("repo", "Start programming!", bot.me.id));
         }
 	});
 
