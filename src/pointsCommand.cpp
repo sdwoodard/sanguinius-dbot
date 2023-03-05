d180b285af596b2ecd1d78c40b75bd5963e66775
Stephen Woodard <stephendwoodard@protonmail.com>
2023-03-05 15:07:56 -0500

    Changed response type

--- src/pointsCommand.cpp
+++ src/pointsCommand.cpp
@@ -32,12 +32,9 @@ void pointsCommand::executeCommand(const dpp::message_create_t& event)
 
    BOOST_LOG_SEV(*mpcLogger, boost::log::trivial::info) << "Produced the message: " << lcMessage.str(); boost::log::core::get()->flush();
 
-   dpp::message lcResponse = dpp::message()
-      .set_content(lcMessage.str())
-      .set_type(dpp::mt_reply)
-      .set_reference(event.msg.id)
-      .set_channel_id(event.msg.channel_id);
-   bot->message_create(lcResponse);
+   dpp::message response_message;
+   response_message.content = lcMessage.str();
+   event.reply(response_message);
 
    BOOST_LOG_SEV(*mpcLogger, boost::log::trivial::info) << "Exiting getPoints"; boost::log::core::get()->flush();
 
